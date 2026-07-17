0de8f8ec457e7f0799acb4d9b2164f78d60016d6
TitasStankus <mr.stankus.titas@gmail.com>
2026-07-17 16:48:31 +0100

    Added project structure and node class

---
+++ Pathfinding Visualizer/Models/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pathfinding_Visualizer.Models
+{
+    public class Node
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+        public bool IsWall { get; set; }
+        public bool BeenVisited { get; set; }
+        public bool IsStart { get; set; }
+        public bool IsEnd { get; set; }
+    }
+}
