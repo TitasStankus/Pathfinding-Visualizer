2e69befcba45ce70fd3063d4359e4aac833d08ff
TitasStankus <mr.stankus.titas@gmail.com>
2026-07-18 20:36:35 +0100

    Created grid generation

--- Pathfinding Visualizer/MainWindow.xaml.cs
+++ Pathfinding Visualizer/MainWindow.xaml.cs
@@ -19,6 +19,25 @@ namespace Pathfinding_Visualizer
         public MainWindow()
         {
             InitializeComponent();
+
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            for (int row=0; row < 20; row++)
+            {
+                for (int column=0; column < 20; column++)
+                {
+                    Border square = new Border();
+
+                    square.BorderBrush = Brushes.Gray;
+                    square.BorderThickness = new Thickness(1);
+                    square.Background = Brushes.White;
+
+                    GridContainer.Children.Add(square);
+                }
+            }
         }
     }
 }
---
+++ Pathfinding Visualizer/Models/GridModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pathfinding_Visualizer.Models
+{
+    public class GridModel
+    {
+        public Node[,] Nodes { get; }
+
+        public GridModel(int rows, int columns)
+        {
+            Nodes = new Node[rows, columns];
+        }
+    }
+}
