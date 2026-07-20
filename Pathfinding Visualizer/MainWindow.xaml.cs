7af406002e8c97d0d61aa672b5717f140bf01aca
TitasStankus <mr.stankus.titas@gmail.com>
2026-07-20 13:09:44 +0100

    Reset path/visited notes with a button

--- Pathfinding Visualizer/MainWindow.xaml.cs
+++ Pathfinding Visualizer/MainWindow.xaml.cs
@@ -17,6 +17,11 @@ namespace Pathfinding_Visualizer
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Path test
+        int lowerX = 0;
+        int lowerY = 0;
+        int upperX = 3;
+
         private int _rows = 20;
         private int _columns = 20;
 
@@ -54,6 +59,12 @@ namespace Pathfinding_Visualizer
 
                     square.BorderBrush = Brushes.Gray;
                     square.BorderThickness = new Thickness(1);
+
+                    // Path test
+                    if (row == lowerY && column >= lowerX && column <= upperX)
+                    {
+                        node.State = NodeState.Path;
+                    }
 
                     UpdateNodeColour(square);
 
@@ -166,6 +177,20 @@ namespace Pathfinding_Visualizer
             GridContainer.Children.Clear();
 
             CreateGrid();
+        }
+
+        private void ResetPath_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Border square in GridContainer.Children)
+            {
+                Node node = (Node)square.Tag;
+                
+                if (node.State == NodeState.Visited || node.State == NodeState.Path)
+                {
+                    node.State = NodeState.Empty;
+                    UpdateNodeColour(square);
+                }
+            }
         }
 
         private void ChangeState(object sender)
